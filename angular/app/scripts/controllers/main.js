ca2fd974ace7c47476f9730bb82723c886940b71
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-03-02 09:45:34 -0500

    first commit

---
+++ angular/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gratefulplace2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Testacular'
+    ];
+  });
