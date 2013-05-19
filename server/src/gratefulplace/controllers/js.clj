c90becf989e0b1c75bac610134f04a84bccbacca
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-05-19 07:29:57 -0400

    log in and log out working better

--- server/src/gratefulplace/controllers/js.clj
+++ server/src/gratefulplace/controllers/js.clj
@@ -4,9 +4,9 @@
 (defn load_session
   [params]
   (let [session-js
-        (fn [username]
+        (fn [value]
           {:body (str "angular.module('gratefulplaceApp').value('loadedSession', " value ")")
            :headers {"content-type" "application/javascript"}})]
     (if-let [auth (friend/current-authentication)]
-      (session-js "{username:'" (:username auth) "'}")
+      (session-js (str "{username:'" (:username auth) "'}"))
       (session-js "null"))))
