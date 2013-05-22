6d6b607750e37caeb86c4954c4320c42192b2256
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-05-22 08:36:49 -0400

    tweaking auth spec

--- server/test/gratefulplace/middleware/t_auth.clj
+++ server/test/gratefulplace/middleware/t_auth.clj
@@ -1,22 +1,21 @@
 (ns gratefulplace.middleware.t-auth
   (:use midje.sweet
         ring.mock.request)
-  (:require [gratefulplace.middleware.auth :as auth]
+  (:require [gratefulplace.db.test :as tdb]
+            [gratefulplace.middleware.auth :as auth]
             [gratefulplace.server :as server]))
 
+(tdb/with-test-db
+  (fact "a successful form login returns a success status code and user info"
+    (server/app
+     (request :post "/login" {:username "flyingmachine"
+                              :password "password"}))
+    => (contains {:status 200}))
 
-(fact "a successful form login returns a success status code and user info"
-  (server/app
-   (request :post "/login" {:username "flyingmachine"
-                            :password "password"}))
-  => (contains {:body "success"
-                :status 200}))
-
-(fact "a failed form login returns a 412"
-  (server/app
-   (request :post "/login" {:username "flyingmachine"
-                            :password "badpass"}))
-  => (contains {:body "failure"
-                :status 412}))
+  (fact "a failed form login returns a 401"
+    (server/app
+     (request :post "/login" {:username "flyingmachine"
+                              :password "badpass"}))
+    => (contains {:status 401})))
 
 
