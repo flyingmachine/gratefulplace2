3f015c4d8fbba0cf392949b4fa4145f42afd7675
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-06-20 20:51:46 -0400

    refactor test helpers to use defnpd

--- server/test/gratefulplace/controllers/test_helpers.clj
+++ server/test/gratefulplace/controllers/test_helpers.clj
@@ -5,6 +5,7 @@
             [gratefulplace.server :as server]
             [clojure.data.json :as json])
   (:use midje.sweet
+        gratefulplace.utils
         [ring.mock.request :only [request header content-type]]))
 
 (defn auth
@@ -20,22 +21,16 @@
                     :current :test})
     request))
 
-(defn req
-  [method path params auth]
+(defnpd req
+  [method path [params nil] [auth nil]]
   (-> (request method path params)
       (content-type "application/json")
       (authenticated auth)))
 
-(defn res
-  "We give it a dummy uri because we're using this with controllers
-and assume that routing has already taken place"
-  ([method path]
-     (res method path nil nil))
-  ([method path params]
-     (res method path params nil))
-  ([method path params auth]
-     (let [params (json/write-str params)]
-       (server/app (req method path params auth)))))
+(defnpd res
+  [method path [params nil] [auth nil]]
+  (let [params (json/write-str params)]
+       (server/app (req method path params auth))))
 
 (defn data
   [response]
@@ -43,13 +38,9 @@ and assume that routing has already taken place"
       :body
       json/read-str))
 
-(defn response-data
-  ([method path]
-     (response-data method path nil nil))
-  ([method path params]
-     (response-data method path params nil))
-  ([method path params auth]
-     (data (res method path params auth))))
+(defnpd response-data
+  [method path [params nil] [auth nil]]
+  (data (res method path params auth)))
 
 (defmacro setup-db-background
   []
