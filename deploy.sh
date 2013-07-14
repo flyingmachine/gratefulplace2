ba2c602a31dfb7c9ad65609ef34d9aedd8586f33
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-07-14 14:12:20 -0400

    dont build on server

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd html-app
+grunt build
+cp -R targets/public ../server/resources/html-app
+cd ../server
+lein uberjar
+cd ..
+cp  server/target/gratefulplace-0.1.0-SNAPSHOT.jar infrastructure/ansible/files/gp2.jar
+cd infrastructure/ansible/
+ansible -i dev deploy.yml
