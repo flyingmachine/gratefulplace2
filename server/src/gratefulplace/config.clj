8f149bbbe20a714beea196824c5e48d033e5d087
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-07-14 11:31:01 -0400

    fixes

--- server/src/gratefulplace/config.clj
+++ server/src/gratefulplace/config.clj
@@ -1,7 +1,8 @@
 (ns gratefulplace.config)
 
 (def conf
-  {:html-paths ["../html-app/app"
+  {:html-paths ["resources/html-app"
+                "../html-app/app"
                 "../html-app/.tmp"]
    :datomic {:db-uri "datomic:free://localhost:4334/gp2"
              :test-uri "datomic:mem://gp2"}
