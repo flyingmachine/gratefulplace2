eabde18efacfc42dbb79a4833dbcb9bf8952d113
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-07-16 22:19:27 -0400

    deploy.sh for production

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+if [ "$#" -eq 0 ]
+then INVENTORY="dev"
+else INVENTORY=$1
+fi
+  
 ./build.sh
 cd infrastructure/ansible/
-ansible-playbook -i dev deploy.yml
+ansible-playbook -i $INVENTORY deploy.yml
