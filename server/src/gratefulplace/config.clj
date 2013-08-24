8cd762076479218e1f1c39a3d35c7b7649e712f5
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-08-24 14:44:47 -0400

    some config

--- server/src/gratefulplace/config.clj
+++ server/src/gratefulplace/config.clj
@@ -9,6 +9,8 @@
      {:html-paths ["html-app"
                    "../html-app/app"
                    "../html-app/.tmp"]
+      :datomic {:db-uri "datomic:free://localhost:4334/gp2"
+                :test-uri "datomic:mem://gp2-test"}
       :moderator-names ["flyingmachine"]
       :send-email false
       :email {:host "smtp.gmail.com"
