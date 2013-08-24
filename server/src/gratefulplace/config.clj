7dfb5d635a250d56a2d7f48edd8d684238300129
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-08-24 15:02:24 -0400

    fixed silly mistake

--- server/src/gratefulplace/config.clj
+++ server/src/gratefulplace/config.clj
@@ -4,19 +4,18 @@
 
 (def conf
   (let [environment (or (env :app-env) "development")]
-    (merge-with
-     merge
+    (merge
      {:html-paths ["html-app"
                    "../html-app/app"
                    "../html-app/.tmp"]
       :datomic {:db-uri "datomic:free://localhost:4334/gp2"
                 :test-uri "datomic:mem://gp2-test"}
       :moderator-names ["flyingmachine"]
-      :send-email false
+      :send-email true
       :email {:host "smtp.gmail.com"
               :from-address "notifications@gratefulplace.com"
-              :from-name "Grateful Place Notifications"}}
-     {:email {:username (env :gp-email-username)
+              :from-name "Grateful Place Notifications"
+              :username (env :gp-email-username)
               :password (env :gp-email-password)}}
      (read-resource (str "config/environments/" environment ".edn")))))
 
