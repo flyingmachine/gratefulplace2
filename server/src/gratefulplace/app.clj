dc00427f965b7f939ad5a924754b7b4b6409735c
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-12-19 11:28:34 -0500

    deployment issues

--- server/src/gratefulplace/app.clj
+++ server/src/gratefulplace/app.clj
@@ -1,11 +1,14 @@
 (ns gratefulplace.app
   (:gen-class)
   (:require [gratefulplace.server :as server]
-            [gratefulplace.db.manage :as db]))
+            [gratefulplace.db.manage :as db]
+            [gratefulplace.config :as config]))
 
 (defn -main
   [cmd]
   (cond 
    (= cmd "server") (server/-main)
    (= cmd "db/reload") (do (println (db/reload)) (System/exit 0))
-   (= cmd "db/migrate") (do (println (db/migrate)) (System/exit 0))))
+   (= cmd "db/seed") (do (println (db/seed)) (System/exit 0))
+   (= cmd "db/migrate") (do (println (db/migrate)) (System/exit 0))
+   (= cmd "config/print-config") (do (config/print-config) (System/exit 0))))
--- server/src/gratefulplace/config.clj
+++ server/src/gratefulplace/config.clj
@@ -9,11 +9,13 @@
       :html-paths ["../html-app/.tmp"
                    "../html-app/app"
                    "html-app"]
-      :datomic {:db-uri "datomic:free://localhost:4334/gp2"
-                :test-uri "datomic:mem://gp2-test"}
       :moderator-names ["flyingmachine"]}
      (read-resource (str "config/environments/" environment ".edn")))))
 
 (defn config
   [& keys]
   (get-in conf keys))
+
+(defn print-config
+  []
+  (println env))
