c07b8f4f14d2742a52f2e3b40f87700801829013
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-12-22 15:28:56 -0500

    tested that forgot password works

--- server/test/gratefulplace/controllers/credential_recovery/t_forgot_password.clj
+++ server/test/gratefulplace/controllers/credential_recovery/t_forgot_password.clj
@@ -12,5 +12,17 @@
   (dj/ent-count :user/password-reset-token)
   => 1)
 
-(fact "consumes token with update"
-  )
+(facts "about token"
+  (res :post "/credential-recovery/forgot-password" {:username (:username (auth))})
+  (let [token (:user/password-reset-token (dj/one :user/password-reset-token))
+        path (str "/credential-recovery/forgot-password/" token)]
+    (fact "returns ok when token exists"
+      (res :get path)
+      => (contains {:status 200}))
+    (fact "returns 501 when token doesn't exist"
+      (res :get "/credential-recovery/forgot-password/xyz")
+      => (contains {:status 400}))
+    (fact "consumes token with update"
+      (res :put path {:new-password "test1234" :new-password-confirmation "test1234"})
+      (dj/ent-count :user/password-reset-token)
+      => 0)))
