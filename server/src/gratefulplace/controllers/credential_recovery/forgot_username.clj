dac40636397557d102a90dcfd58a174c87175ff9
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-12-22 15:49:28 -0500

    move to lib templates

--- server/src/gratefulplace/controllers/credential_recovery/forgot_username.clj
+++ server/src/gratefulplace/controllers/credential_recovery/forgot_username.clj
@@ -3,19 +3,15 @@
             [gratefulplace.db.validations :as validations]
             [gratefulplace.email.sending.senders :as email]
             [gratefulplace.controllers.shared :refer :all]
-            [liberator.core :refer [defresource]]
+            [com.flyingmachine.liberator-templates.sets.json-crud
+             :refer (defshow defcreate! defupdate!)]
             [flyingmachine.webutils.utils :refer :all]))
 
-(defresource create! [params]
-  :allowed-methods [:post]
-  :available-media-types ["application/json"]
-  
-  :malformed? (validator params validations/forgot-username)
-  :handle-malformed errors-in-ctx
-
+(defcreate!
+  [params]
+  :invalid? (validator params validations/forgot-username)
   :exists? (fn [_] (exists? (seq (dj/all [:user/email (:email params)]))))
   :can-post-to-missing? false
   :handle-not-found (fn [_] {:errors {:email ["That email address doesn't exist"]}})
-  
   :post! (fn [ctx] (future (email/send-forgot-username (:record ctx))))
-  :handle-created {})
+  :return {})
