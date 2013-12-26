892c029121702e185ce276f7046e7a0980b57a96
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-12-26 10:16:31 -0500

    remove extracted files

--- server/src/gratefulplace/controllers/session.clj
+++
@@ -1,7 +0,0 @@
-(ns gratefulplace.controllers.session
-  (:require [cemerick.friend :as friend]))
-
-(defn create!
-  [params]
-  (if-let [auth (friend/current-authentication)]
-    {:body (select-keys auth [:username :id :about])}))
