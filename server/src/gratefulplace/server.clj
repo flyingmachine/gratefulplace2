183871b0618c895141d33dc7c0497d7f76e614c4
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-01-04 10:45:01 -0500

    remove unused

--- server/src/gratefulplace/server.clj
+++ server/src/gratefulplace/server.clj
@@ -1,14 +1,9 @@
 (ns gratefulplace.server
   (:gen-class)
   (:require [compojure.core :as compojure]
-            [rabble.ring-app :as ring-app]
-            [rabble.middleware.routes :as routes]
-            [rabble.middleware.auth :refer (auth)]
+            [rabble.ring-app :refer (app)]
             [ring.adapter.jetty :refer (run-jetty)]
             [gratefulplace.config :refer (config)]))
-
-(def app (ring-app/wrap
-          (auth (compojure/routes routes/auth-routes routes/rabble-routes))))
 
 (defn start
   "Start the jetty server"
